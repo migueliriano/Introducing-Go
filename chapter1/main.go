7ca0e1b1f0cdc93bb0d91a1bdb87b4971299a9ff
Miguel <mliriano@intellisys.com.do>
2017-02-23 11:19:30 -0400

    complete chapter2 and refactor chapter1 exercise

--- chapter1/main.go
+++ chapter1/main.go
@@ -20,24 +20,23 @@ func main() {
  *
  * 1. What is whitespace?
  *
- * - It's a character does not correspond to a visible mark, but typically
- *   does occupy an area on a page. On Go languages is removed
- *   when the program is executed.
+ *    - It's a character does not correspond to a visible mark, but typically
+ *      does occupy an area on a page.
  *
  * 2. What is a comment? What are the two ways of writing a comment?
  *
- * - The comment is the way you can add the description and documentation
- *   of what your programn do The commands are not execute.
+ *    - The comment is the way you can add the description and documentation
+ *      of what your programn do The commands are not execute.
  *
- * - We can write inline and block comments.
+ *    - We can write inline and block comments.
  *
  * 3. Our program began with package main. What would the files in the fmt package
  * begin with?
  *
- * - package fmt
+ *    - package fmt
  *
  * 4. We used the Println function defined in the fmt package. If you wanted to use
  *   the Exit function from the os package, what would you need to do?
  *
- * - We need to defined os in the begin of this file e.i: import "os"
+ *    - We need to defined os in the begin of this file e.i: import "os"
  */
--- chapter2/main.go
+++ chapter2/main.go
@@ -46,14 +46,25 @@ func main() {
  *
  * 1. How are integers stored on a computer?
  *
+ *    - On the computer the integers are stored on 2 digits (1 0) [Binary]
+ *
  * 2. We know that (in base 10) the largest one-digit number is 9
  *    and the largest twodigit number is 99. Given that in binary the largest
  *    two-digit number is 11 (3), the largest three-digit number is 111 (7) and
  *    the largest four-digit number is 1111 (15), what’s the largest eight-digit number?
  *
+ *    - 11111111 (255)
+ *
  * 3. What is a string? How do you find its length?
+ *
+ *    - String is asequence of characters with a definite length used to represent text.
+ *
+ *    - You can find its length using a native function of Go "len".
+ *      i.e.: len("Miguel")
  *
  *
  * 4. What’s the value of the expression (true && false) || (false && true) || !
  *   (false && false)?
+ *
+ *   - TRUE
  */
