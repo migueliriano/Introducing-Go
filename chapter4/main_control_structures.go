b9fe3584521b0da0093cc16bb470176da90c00f2
Miguel <mliriano@intellisys.com.do>
2017-03-06 17:40:16 -0400

    added chapter4

---
+++ chapter4/main_control_structures.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+//Control Structures
+func main() {
+
+	//For Loop even and odd example with if
+	for i := 1; i <= 10; i++ {
+
+		if i%2 == 0 {
+			fmt.Println(i, "even")
+		} else {
+			fmt.Println(i, "odd")
+		}
+	}
+
+	var numberToLetter int32
+
+	fmt.Println("Insert a number to covert it into a Word")
+
+	fmt.Scanf("%d", &numberToLetter)
+
+	switch numberToLetter {
+	case 0:
+		fmt.Println("Zero")
+	case 1:
+		fmt.Println("One")
+	case 2:
+		fmt.Println("Two")
+	case 3:
+		fmt.Println("Three")
+	case 4:
+		fmt.Println("Four")
+	default:
+		fmt.Println("Not number found")
+	}
+}
